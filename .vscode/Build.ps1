a2579292d5368879f2ed7faebdc680ff35b8333c
Dennis Westerman <71754369+denniswesterman@users.noreply.github.com>
2024-05-16 15:39:11 +0200

    cc

--- .vscode/Build.ps1
+++ .vscode/Build.ps1
@@ -1,34 +1,28 @@
-# Vars
-# . ".vscode\Global.ps1"
+# Variables
+. ".vscode\Global.ps1"
 
-# intunewin
-[string]$Uri = "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/blob/master"
+# IntuneWin tool
+[string]$Uri = "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/raw/master"
 [string]$Exe = "IntuneWinAppUtil.exe"
-
 
 $win32appFolderPath = "C:\Temp\win32app"
 
+# Ensure the folder path exists and clean it if necessary
 if (Test-Path $win32appFolderPath) {
     Remove-Item -Path $win32appFolderPath -Recurse -Force
 }
 
 New-Item -ItemType Directory -Path $win32appFolderPath -Force
 
-# Source content prep tool
-if (-not(Test-Path -Path "$win32appFolderPath\$Exe")){
+# Download the content prep tool if it does not exist
+if (-not (Test-Path -Path "$win32appFolderPath\$Exe")) {
     Invoke-WebRequest -Uri "$Uri/$Exe" -OutFile "$win32appFolderPath\$Exe"
 }
-$currentDirectory = Get-Location
-# Execute content prep tool
-$processOptions = @{
-    FilePath = "$win32appFolderPath\$Exe"
-    ArgumentList  = "-c ""$Cache"" -s ""$Cache\Deploy-Application.exe"" -o ""$win32appFolderPath"" -q"
-    WindowStyle = "Maximized"
-    Wait = $true
-}
 
-Start-Process @processOptions
+$IntuneUtilPath = "$win32appFolderPath\$Exe"
 
-# Rename and prepare for upload
-Move-Item -Path "$env:TEMP\Deploy-Application.intunewin" -Destination "$Desktop\$Application.intunewin" -Force -Verbose
-explorer $Desktop
+
+# Run the IntuneWinAppUtil tool
+Set-Location -Path $win32appFolderPath
+& $IntuneUtilPath -c $ToolkitPath -s $InstallexeName -o $win32appFolderPath -q
+
--- .vscode/Global.ps1
+++ .vscode/Global.ps1
@@ -1,3 +1,20 @@
+# Get the current directory
+$currentDirectory = Get-Location
+Write-Output "Current Directory: $currentDirectory"
+
+# Set the correct path to the Toolkit directory
+$ToolkitPath = "$currentDirectory\Toolkit"
+if (-not (Test-Path -Path $ToolkitPath)) {
+    Write-Error "The path $ToolkitPath does not exist."
+    exit
+}
+
+# Set the source path and installation script based on the current directory
+$AppFullName = Get-Item -Path $ToolkitPath
+$Installexe = Get-ChildItem -Path $AppFullName.FullName -Filter *.exe
+$InstallexeName = $Installexe.Name
+
+<#
 [string]$Desktop = [Environment]::GetFolderPath('DesktopDirectory')
 [string]$WDADesktop = "C:\Users\WDAGUtilityAccount\Desktop"
 [string]$Win32App = "C:\M365\win32app"
@@ -9,3 +26,4 @@
 Remove-Item -Path "$Win32App" -Recurse -Force -ErrorAction Ignore
 Copy-Item -Path "Toolkit" -Destination "$Cache" -Recurse -Force -Verbose -ErrorAction Ignore
 explorer "$Cache"
+#>
